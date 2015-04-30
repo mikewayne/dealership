abe2c908c9945c0c6fb8f66b3ce35a612ccbdb63
mikewayne <mhoeft1@my.chemeketa.edu>
2015-04-30 13:15:07 -0700

    Update dealership.rb

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,12 +1,50 @@
-require('sinatra')
-require('sinatra/reloader')
-also_reload('lib/**/*.rb')
-require('./lib/vehicle')
-require('./lib/dealership')
-
-get('/') do
-  erb(:index)
-end
-
-get('') do
-  erb(:dealerships_form)
+class Dealership
+  
+  @@dealerships = []
+  
+  define_method(:initialize) do |name|
+    @name = name
+    @id = @@dealerships.length().+(1)
+    @cars = []
+  end
+  
+  define_method(:name) do
+    @name
+  end
+  
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+  
+  define_singleton_method(:find) do |identification|
+    found_dealership = nil
+    @@dealerships.each() do |dealership|
+      if dealership.id().eql?(identification.to_i())
+        found_dealership = dealership
+      end
+    end
+    found_dealership
+  end
+  
+  define_method(:save) do
+    @@dealerships.push(self)
+  end
+  
+  define_method(:id) do
+    @id
+  end
+  
+  define_singleton_method(:clear) do
+    @@dealerships = []
+  end
+  
+  define_method(:add_car) do |vehicle|
+    @cars.push(vehicle)
+  end
+  
+  define_method(:cars) do
+    @cars
+  end
+  
+end  
+  
