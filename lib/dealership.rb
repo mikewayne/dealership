bce469ac5db3e715ca8e6ff98c67dc7b586b4c24
Debbie Balbo & Michael Wayne Hoeft <student@epicodus.com>
2015-04-29 17:27:47 -0700

    Working on more files, rspecs, etc.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,44 +1,12 @@
-class Vehicle
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/vehicle')
+require('./lib/dealership')
 
-  @@vehicles = []
-
-  define_method(:initialize) do |make, model, year|
-    @make = make
-    @model = model
-    @year = year
-  end
-
-  define_method(:make) do
-    @make
-  end
-
-  define_method(:model) do
-    @model
-  end
-
-  define_method(:year) do
-    @year
-  end
-
-  define_singleton_method(:all) do
-    @@vehicles
-  end
-
-  define_method(:save) do
-    @@vehicles.push(self)
-  end
-
-  define_singleton_method(:clear) do
-    @@vehicles = []
-  end
-
-  define_method(:age) do
-    current_year = Time.new().year()
-    age = current_year.-(@year)
-  end
-
-  define_method(:worth_buying?) do
-    american_cars = ["Chrysler", "Ford", "GM"]
-    american_cars.include?(@make).&(self.age.<=(15))
-  end
+get('/') do
+  erb(:index)
 end
+
+get('') do
+  erb(:dealerships_form)
