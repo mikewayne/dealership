698398d59481d09f81590657ef5afc4be3b910df
mikewayne <mhoeft1@my.chemeketa.edu>
2015-04-30 13:37:28 -0700

    Update dealership_integration_spec.rb

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -2,7 +2,12 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 
-describe('', {:type => :feature}) do
-  it('') do
+describe('integration spec for dealership circuitry', {:type => :feature}) do
+  it('user enters in new dealership info') do
     visit('/')
-    fill_in
+    click_link('Add New Dealership')
+    fill_in('name', :with => '')
+    click_button('')
+    expect(page).to have_content('')
+  end
+end  
